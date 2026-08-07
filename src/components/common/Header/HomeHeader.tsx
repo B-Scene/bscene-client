73870009360324da51b222a66d841d72d7537338
yangyangeeee <sophia_ysy@naver.com>
2026-08-07 17:52:47 +0900

    [Fix] 캘린더 아이콘 위치 변경 (헤더로)

--- src/components/common/Header/HomeHeader.tsx
+++ src/components/common/Header/HomeHeader.tsx
@@ -3,12 +3,15 @@ import BSceneLogo from "@/assets/bscene-logo.svg";
 
 type HomeHeaderProps = {
   rightAction: ReactNode;
+  leftAction?: ReactNode;
 };
 
-export const HomeHeader = ({ rightAction }: HomeHeaderProps) => {
+export const HomeHeader = ({ rightAction, leftAction }: HomeHeaderProps) => {
   return (
     <header className="-mx-5 box-border grid h-12 w-[393px] max-w-[calc(100%+40px)] grid-cols-[1fr_auto_1fr] items-center bg-neutral-0 px-6">
-      <span aria-hidden="true" />
+      <div className="flex items-center gap-4 justify-self-start [&>button]:flex [&>button]:size-6 [&>button]:items-center [&>button]:justify-center [&_svg]:size-6">
+        {leftAction}
+      </div>
 
       <img
         src={BSceneLogo}
